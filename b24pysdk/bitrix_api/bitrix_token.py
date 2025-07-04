04dac3ee41f260da4fb2a8d52733e82f21adf659
lra <lra@it-solution.ru>
2025-07-05 01:54:39 +0300

    updated bitrix_token.py

--- b24pysdk/bitrix_api/bitrix_token.py
+++ b24pysdk/bitrix_api/bitrix_token.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Optional, Sequence, Text, Union
+from typing import Callable, Dict, Optional, Sequence, Text, Tuple, Union
 
+from ..error import BitrixAPIExpiredToken
 from ..utils.types import B24BatchRequestData, JSONDict, Key, Timeout
+from .bitrix_app import BitrixApp
+from .functions import call_batch, call_batches, call_method
+from .oauth_requester import OAuthRequester
 
-from .functions import call_method, call_batch, call_batches
 
-
-class BaseBitrixToken(ABC):
+class AbstractBitrixToken(ABC):
     """"""
 
     domain: Text = NotImplemented
@@ -15,7 +17,10 @@ class BaseBitrixToken(ABC):
     auth_token: Text = NotImplemented
     """"""
 
-    is_webhook: bool = NotImplemented
+    refresh_token: Text = NotImplemented
+    """"""
+
+    bitrix_app: Optional[BitrixApp] = NotImplemented
     """"""
 
     @abstractmethod
@@ -23,21 +28,63 @@ class BaseBitrixToken(ABC):
         """"""
         super().__init__(*args, **kwargs)
 
+    @property
+    def is_webhook(self) -> bool:
+        """"""
+        return not bool(self.bitrix_app)
+
+    @property
+    def oauth_requester(self) -> OAuthRequester:
+        """"""
+        return OAuthRequester(self.bitrix_app)
+
+    @property
+    def _auth_data(self) -> Dict:
+        """"""
+        return dict(
+            domain=self.domain,
+            auth_token=self.auth_token,
+            is_webhook=self.is_webhook,
+        )
+
+    def _call_with_refresh(
+            self,
+            call_func: Callable,
+            parameters: Dict,
+            auto_refresh: bool = True,
+    ) -> JSONDict:
+        """"""
+        try:
+            return call_func(**self._auth_data, **parameters)
+        except BitrixAPIExpiredToken as error:
+            if auto_refresh and not self.is_webhook:
+                self.auth_token, self.refresh_token = self.refresh()
+                return call_func(**self._auth_data, **parameters)
+            else:
+                raise error
+
+    def refresh(self) -> Tuple[Text, Text]:
+        """"""
+        json_response = self.oauth_requester.refresh(refresh_token=self.refresh_token)
+        return json_response["access_token"], json_response["refresh_token"]
+
     def call_method(
             self,
             api_method: Text,
             params: Optional[JSONDict] = None,
             *,
+            auto_refresh: bool = True,
             timeout: Timeout = None,
     ) -> JSONDict:
         """"""
-        return call_method(
-            domain=self.domain,
-            api_method=api_method,
-            auth_token=self.auth_token,
-            is_webhook=self.is_webhook,
-            params=params,
-            timeout=timeout,
+        return self._call_with_refresh(
+            call_func=call_method,
+            parameters=dict(
+                api_method=api_method,
+                params=params,
+                timeout=timeout,
+            ),
+            auto_refresh=auto_refresh,
         )
 
     def call_batch(
@@ -46,17 +93,19 @@ class BaseBitrixToken(ABC):
             *,
             halt: bool = False,
             ignore_size_limit: bool = False,
+            auto_refresh: bool = True,
             timeout: Timeout = None,
     ) -> JSONDict:
         """"""
-        return call_batch(
-            methods=methods,
-            domain=self.domain,
-            auth_token=self.auth_token,
-            halt=halt,
-            is_webhook=self.is_webhook,
-            ignore_size_limit=ignore_size_limit,
-            timeout=timeout,
+        return self._call_with_refresh(
+            call_func=call_batch,
+            parameters=dict(
+                methods=methods,
+                halt=halt,
+                ignore_size_limit=ignore_size_limit,
+                timeout=timeout,
+            ),
+            auto_refresh=auto_refresh,
         )
 
     def call_batches(
@@ -64,28 +113,43 @@ class BaseBitrixToken(ABC):
             methods: Union[Dict[Key, B24BatchRequestData], Sequence[B24BatchRequestData]],
             *,
             halt: bool = False,
+            auto_refresh: bool = True,
             timeout: Timeout = None,
     ) -> JSONDict:
         """"""
-        return call_batches(
-            methods=methods,
-            domain=self.domain,
-            auth_token=self.auth_token,
-            halt=halt,
-            is_webhook=self.is_webhook,
-            timeout=timeout,
+        return self._call_with_refresh(
+            call_func=call_batches,
+            parameters=dict(
+                methods=methods,
+                halt=halt,
+                timeout=timeout,
+            ),
+            auto_refresh=auto_refresh,
         )
 
 
-class BitrixToken(BaseBitrixToken):
+class BitrixToken(AbstractBitrixToken):
     """"""
 
     def __init__(
             self,
             domain: Text,
             auth_token: Text,
-            is_webhook: bool = False,
+            refresh_token: Optional[Text] = None,
+            bitrix_app: Optional[BitrixApp] = None,
     ):
         self.domain = domain
         self.auth_token = auth_token
-        self.is_webhook = is_webhook
+        self.refresh_token = refresh_token
+        self.bitrix_app = bitrix_app
+
+
+class BitrixWebhook(BitrixToken):
+    """"""
+
+    def __init__(
+            self,
+            domain: Text,
+            auth_token: Text,
+    ):
+        super().__init__(domain, auth_token)
