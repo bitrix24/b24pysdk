ee7bc2d2d3b400bbdb7385a21f94d2a351b26f21
lra <lra@it-solution.ru>
2025-07-09 19:19:32 +0300

    updated _client.py

--- b24pysdk/_client.py
+++ b24pysdk/_client.py
@@ -1,8 +1,9 @@
-from typing import Sequence
+from typing import Dict, Sequence, Union
 
 from . import scopes
-from ._bitrix_api_request import BitrixAPIRequest
 from .bitrix_api.bitrix_token import AbstractBitrixToken
+from .bitrix_api.classes import BitrixAPIBatchesRequest, BitrixAPIBatchRequest, BitrixAPIRequest
+from .utils.types import Key, Timeout
 
 
 class Client:
@@ -17,8 +18,32 @@ class Client:
         self.bitrix_token = bitrix_token
         self.crm = scopes.CRM(self)
 
-    def call_batch(self, bitrix_api_requests: Sequence[BitrixAPIRequest]):
+    def call_batch(
+            self,
+            bitrix_api_requests: Union[Dict[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> BitrixAPIBatchRequest:
         """"""
+        return BitrixAPIBatchRequest(
+            bitrix_token=self.bitrix_token,
+            bitrix_api_requests=bitrix_api_requests,
+            halt=halt,
+            ignore_size_limit=ignore_size_limit,
+            timeout=timeout,
+        )
 
-    def call_batches(self, bitrix_api_requests: Sequence[BitrixAPIRequest]):
+    def call_batches(
+            self,
+            bitrix_api_requests: Union[Dict[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
+            halt: bool = False,
+            timeout: Timeout = None,
+    ):
         """"""
+        return BitrixAPIBatchesRequest(
+            bitrix_token=self.bitrix_token,
+            bitrix_api_requests=bitrix_api_requests,
+            halt=halt,
+            timeout=timeout,
+        )
