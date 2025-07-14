dd5c1a68732645c2c5b817461d2bbc9a3e4cff86
lra <lra@it-solution.ru>
2025-07-14 09:47:34 +0300

    added BitrixAPIListRequest

--- b24pysdk/bitrix_api/classes/__init__.py
+++ b24pysdk/bitrix_api/classes/__init__.py
@@ -1,12 +1,15 @@
 from .bitrix_api_batch_request import BitrixAPIBatchesRequest, BitrixAPIBatchRequest
+from .bitrix_api_list_request import BitrixAPIListRequest
 from .bitrix_api_request import BitrixAPIRequest
-from .bitrix_api_response import BitrixAPIResponse
+from .bitrix_api_response import BitrixAPIListResponse, BitrixAPIResponse
 from .bitrix_api_response_time import BitrixAPIResponseTime
 
 __all__ = [
     "BitrixAPIBatchesRequest",
     "BitrixAPIBatchRequest",
+    "BitrixAPIListRequest",
     "BitrixAPIRequest",
+    "BitrixAPIListResponse",
     "BitrixAPIResponse",
     "BitrixAPIResponseTime",
 ]
---
+++ b24pysdk/bitrix_api/classes/bitrix_api_list_request.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+from ...utils.types import JSONDict, JSONList
+from ..bitrix_token import AbstractBitrixToken
+from .bitrix_api_request import BitrixAPIRequest
+from .bitrix_api_response import BitrixAPIListResponse
+
+
+class BitrixAPIListRequest(BitrixAPIRequest):
+    """"""
+
+    __slots__ = ("_limit",)
+
+    _response: Optional[BitrixAPIListResponse]
+    _limit: Optional[int]
+
+    def __init__(
+            self,
+            *,
+            bitrix_token: AbstractBitrixToken,
+            bitrix_api_request: BitrixAPIRequest,
+            limit: Optional[int] = None,
+    ):
+        super().__init__(
+            bitrix_token=bitrix_token,
+            api_method=bitrix_api_request.api_method,
+            params=bitrix_api_request.params,
+            timeout=bitrix_api_request.timeout,
+        )
+        self._limit = limit
+
+    @property
+    def response(self) -> BitrixAPIListResponse:
+        return self._response or self.execute()
+
+    @property
+    def result(self) -> JSONList:
+        return self.response.result
+
+    def _call(self) -> JSONDict:
+        """"""
+        return self._bitrix_token.call_list(
+            api_method=self._api_method,
+            params=self._params,
+            limit=self._limit,
+            timeout=self._timeout,
+        )
+
+    def execute(self) -> BitrixAPIListResponse:
+        """"""
+        self._response = BitrixAPIListResponse.from_dict(self._call())
+        return self._response
