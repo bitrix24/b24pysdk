cebd653b0d5328690eb106c830960f7f2314fb64
lra <lra@it-solution.ru>
2025-08-19 13:47:10 +0300

    added new constants

--- b24pysdk/_constants.py
+++ b24pysdk/_constants.py
@@ -1,11 +1,16 @@
 import sys
+from typing import Final
+
+from .utils.types import DefaultTimeout, Number
 
 PYTHON_VERSION = sys.version_info
 
-DEFAULT_TIMEOUT = 10
+DEFAULT_TIMEOUT: Final[DefaultTimeout] = 10
 
-MAX_RETRIES = 3
+INITIAL_RETRY_DELAY: Final[Number] = 1
 
-INITIAL_RETRY_DELAY = 1
+MAX_BATCH_SIZE: Final[int] = 50
 
-RETRY_DELAY_INCREMENT = 0
+MAX_RETRIES: Final[int] = 3
+
+RETRY_DELAY_INCREMENT: Final[Number] = 0
