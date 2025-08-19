c6ad06e1544fdab60db08eb23aa5d0ae63c406b3
lra <lra@it-solution.ru>
2025-08-19 13:34:22 +0300

    added py.typed

---
+++ b24pysdk/__main__.py
@@ -0,0 +1,10 @@
+from b24pysdk.version import VERSION
+
+
+def main():
+    """Prints version information for python-telegram-bot, the Bot API and Python."""
+    print(f"b24pysdk version {VERSION}")
+
+
+if __name__ == "__main__":
+    main()
