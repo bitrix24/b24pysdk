dffe3782aed063f46d1f75e326973b1489e14e02
lra <lra@it-solution.ru>
2025-09-02 19:09:50 +0300

    updated methods in Client

--- b24pysdk/_client.py
+++ b24pysdk/_client.py
@@ -1,8 +1,8 @@
-from typing import Dict, Optional, Sequence, Union
+from typing import Dict, Mapping, Sequence, Union, overload
 
 from . import scopes
 from .bitrix_api.bitrix_token import AbstractBitrixToken
-from .bitrix_api.classes import BitrixAPIBatchesRequest, BitrixAPIBatchRequest, BitrixAPIListRequest, BitrixAPIRequest
+from .bitrix_api.classes import BitrixAPIBatchesRequest, BitrixAPIBatchRequest, BitrixAPIRequest
 from .utils.types import Key, Timeout
 
 
@@ -11,20 +11,27 @@ class Client:
 
     __slots__ = (
         "_bitrix_token",
+        "_kwargs",
         "crm",
+        "department",
         "user",
     )
 
     _bitrix_token: AbstractBitrixToken
+    _kwargs: Dict
     crm: scopes.CRM
+    department: scopes.Department
     user: scopes.User
 
     def __init__(
             self,
             bitrix_token: AbstractBitrixToken,
+            **kwargs,
     ):
         self._bitrix_token = bitrix_token
+        self._kwargs = kwargs
         self.crm = scopes.CRM(self)
+        self.department = scopes.Department(self)
         self.user = scopes.User(self)
 
     def __str__(self):
@@ -33,9 +40,27 @@ class Client:
     def __repr__(self):
         return f"{self.__class__.__name__}(bitrix_token={self._bitrix_token})"
 
+    @overload
     def call_batch(
             self,
-            bitrix_api_requests: Union[Dict[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
+            bitrix_api_requests: Mapping[Key, BitrixAPIRequest],
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> BitrixAPIBatchRequest: ...
+
+    @overload
+    def call_batch(
+            self,
+            bitrix_api_requests: Sequence[BitrixAPIRequest],
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> BitrixAPIBatchRequest: ...
+
+    def call_batch(
+            self,
+            bitrix_api_requests: Union[Mapping[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
             halt: bool = False,
             ignore_size_limit: bool = False,
             timeout: Timeout = None,
@@ -47,11 +72,28 @@ class Client:
             halt=halt,
             ignore_size_limit=ignore_size_limit,
             timeout=timeout,
+            **self._kwargs,
         )
+
+    @overload
+    def call_batches(
+            self,
+            bitrix_api_requests: Mapping[Key, BitrixAPIRequest],
+            halt: bool = False,
+            timeout: Timeout = None,
+    ) -> BitrixAPIBatchRequest: ...
+
+    @overload
+    def call_batches(
+            self,
+            bitrix_api_requests: Sequence[BitrixAPIRequest],
+            halt: bool = False,
+            timeout: Timeout = None,
+    ) -> BitrixAPIBatchRequest: ...
 
     def call_batches(
             self,
-            bitrix_api_requests: Union[Dict[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
+            bitrix_api_requests: Union[Mapping[Key, BitrixAPIRequest], Sequence[BitrixAPIRequest]],
             halt: bool = False,
             timeout: Timeout = None,
     ) -> BitrixAPIBatchesRequest:
@@ -61,16 +103,5 @@ class Client:
             bitrix_api_requests=bitrix_api_requests,
             halt=halt,
             timeout=timeout,
-        )
-
-    def call_list(
-            self,
-            bitrix_api_request: BitrixAPIRequest,
-            limit: Optional[int] = None,
-    ) -> BitrixAPIListRequest:
-        """"""
-        return BitrixAPIListRequest(
-            bitrix_token=self._bitrix_token,
-            bitrix_api_request=bitrix_api_request,
-            limit=limit,
+            **self._kwargs,
         )
