a4ae02ba25d51ed1a2f789c702d0220000b85d58
lra <lra@it-solution.ru>
2025-11-27 18:10:59 +0300

    fixed docs in __main__.py

--- b24pysdk/__main__.py
+++ b24pysdk/__main__.py
@@ -2,7 +2,7 @@ from b24pysdk.version import SDK_VERSION
 
 
 def main():
-    """Prints version information for python-telegram-bot, the Bot API and Python."""
+    """Prints version information for B24PySDK - an official Python library for the Bitrix24 REST API."""
     print(f"b24pysdk version {SDK_VERSION}")
 
 
