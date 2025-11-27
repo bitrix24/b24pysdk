f2263d80b31e0f94d0269ef681b08aedd7eb0c86
lra <lra@it-solution.ru>
2025-11-27 18:16:35 +0300

    fixed protocols

--- b24pysdk/bitrix_api/protocols/__init__.py
+++ b24pysdk/bitrix_api/protocols/__init__.py
@@ -1,7 +1,8 @@
 from .bitrix_oauth_protocol import BitrixOAuthProtocol
-from .bitrix_token_protocol import BitrixTokenProtocol
+from .bitrix_token_protocol import BitrixTokenFullProtocol, BitrixTokenProtocol
 
 __all__ = [
     "BitrixOAuthProtocol",
+    "BitrixTokenFullProtocol",
     "BitrixTokenProtocol",
 ]
--- b24pysdk/bitrix_api/protocols/bitrix_token_protocol.py
+++ b24pysdk/bitrix_api/protocols/bitrix_token_protocol.py
@@ -1,21 +1,86 @@
-from typing import Optional, Protocol, Text
+from typing import Mapping, Optional, Protocol, Sequence, Text, overload
 
-from ...utils.types import JSONDict, Timeout
+from ...utils.types import B24BatchMethods, B24BatchMethodTuple, JSONDict, Key, Timeout
 
 
 class BitrixTokenProtocol(Protocol):
     """"""
 
     domain: Text
-    is_webhook: bool
-    auth_token: Text
-    refresh_token: Optional[Text]
 
     def call_method(
             self,
             api_method: Text,
             params: Optional[JSONDict] = None,
-            *,
             timeout: Timeout = None,
-            **kwargs,
+    ) -> JSONDict: ...
+
+
+class BitrixTokenFullProtocol(BitrixTokenProtocol, Protocol):
+    """"""
+
+    @overload
+    def call_batch(
+            self,
+            methods: Mapping[Key, B24BatchMethodTuple],
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    @overload
+    def call_batch(
+            self,
+            methods: Sequence[B24BatchMethodTuple],
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    def call_batch(
+            self,
+            methods: B24BatchMethods,
+            halt: bool = False,
+            ignore_size_limit: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    @overload
+    def call_batches(
+            self,
+            methods: Mapping[Key, B24BatchMethodTuple],
+            halt: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    @overload
+    def call_batches(
+            self,
+            methods: Sequence[B24BatchMethodTuple],
+            halt: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    def call_batches(
+            self,
+            methods: B24BatchMethods,
+            halt: bool = False,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    def call_list(
+            self,
+            api_method: Text,
+            params: Optional[JSONDict] = None,
+            limit: Optional[int] = None,
+            timeout: Timeout = None,
+    ) -> JSONDict: ...
+
+    def call_list_fast(
+            self,
+            api_method: Text,
+            params: Optional[JSONDict] = None,
+            descending: bool = False,
+            limit: Optional[int] = None,
+            timeout: Timeout = None,
     ) -> JSONDict: ...
