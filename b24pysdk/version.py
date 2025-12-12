b4646f7fa1a01abb5a309710ba2249c0ac6928bd
lra <lra@it-solution.ru>
2025-12-12 18:14:18 +0300

    release 0.3.0a1

--- b24pysdk/version.py
+++ b24pysdk/version.py
@@ -2,5 +2,10 @@ from typing import Final, Text
 
 from ._version import __title__, __version__
 
+__all__ = [
+    "SDK_NAME",
+    "SDK_VERSION",
+]
+
 SDK_NAME: Final[Text] = __title__
 SDK_VERSION: Final[Text] = __version__
