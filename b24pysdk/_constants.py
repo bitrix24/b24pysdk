60d871b45647154fa4430521959e1704fab6f915
lra <lra@it-solution.ru>
2025-12-12 17:37:06 +0300

    updated _constants.py

--- b24pysdk/_constants.py
+++ b24pysdk/_constants.py
@@ -1,5 +1,11 @@
 import sys
-from typing import Final, Tuple
+from typing import Final, Text, Tuple
+
+__all__ = [
+    "MAX_BATCH_SIZE",
+    "PYTHON_VERSION",
+    "TEXT_PYTHON_VERSION",
+]
 
 MAX_BATCH_SIZE: Final[int] = 50
 """"""
@@ -7,5 +13,5 @@ MAX_BATCH_SIZE: Final[int] = 50
 PYTHON_VERSION: Final[Tuple] = sys.version_info
 """"""
 
-TEXT_PYTHON_VERSION = f"{PYTHON_VERSION[0]}.{PYTHON_VERSION[1]}.{PYTHON_VERSION[2]}"
+TEXT_PYTHON_VERSION: Final[Text] = f"{PYTHON_VERSION[0]}.{PYTHON_VERSION[1]}.{PYTHON_VERSION[2]}"
 """"""
